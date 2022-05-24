6d6c1282c28ac9c290ad6a991b9b09e5f3110137
stephan-lopes <40838038+stephan-lopes@users.noreply.github.com>
2022-05-23 22:48:46 -0300

    [fix] Corrigindo coluna com nome errado

--- models/update.go
+++ models/update.go
@@ -9,7 +9,7 @@ func Update(id int64, todo Todo) (int64, error) {
 	}
 	defer conn.Close()
 
-	result, err := conn.Exec(`UPDATE todos SET title = $1, description = $2, completed = $3 WHERE id = $4`, todo.Title, todo.Description, todo.Completed, id)
+	result, err := conn.Exec(`UPDATE todos SET title = $1, description = $2, done = $3 WHERE id = $4`, todo.Title, todo.Description, todo.Completed, id)
 	if err != nil {
 		return 0, err
 	}
